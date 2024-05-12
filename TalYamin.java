1e2e4abd8c5fba15724a006d3e95084f306a5c97
Tal Yamin <talym@mta.ac.il>
2024-05-12 20:24:40 +0300

    Tal is updated

--- TalYamin.java
+++ TalYamin.java
@@ -3,7 +3,7 @@ public class TalYamin{
 public static void main(String[] args) {
 		for (int i=1;i<=10; i++) 
 		{ 
-		System.out.println("Tal Yamin ..."+i); 
+		System.out.println("Tal Yamin is updated now ..."+i); 
 		} 
 	} 
 } 
